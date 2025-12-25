a7662e657145111b55326df73b5610cd8243a95c
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-26 00:16:37 +0600

    main class added

---
+++ main.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative 'mastermind_host'
+require_relative 'mastermind_knuth'
+require_relative 'mastermind'
+
+puts 'Choose one:-'
+puts 'Enter 1 if you want Computer to Host'
+puts 'Enter 2 if you want to be the Host and Computer guess randomly'
+puts "'Enter 3 if you want to be the Host and Computer guess according to Knuth's algorithm"
+
+input = gets.chomp
+
+case input
+
+when 1
+  Mastermind.new.play
+when 2
+  MastermindHost.new.play
+when 3
+  MastermindKnuth.new.play
+else
+  puts 'Invalid input!'
+end
