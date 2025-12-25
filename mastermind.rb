bc1958e0d6ca85717de515fabcfe02a1af349466
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-25 16:45:20 +0600

    main class constructor created

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
-module Mastermind
+# MAIN CLASS
+class Mastermind
+  COLORS = %w[red green blue orange yellow purple].freeze
+  def initialize(code_length = 4, max_turns= 12)
+    @code_length = code_length
+    @max_turns = max_turns
+    @secret_code = Array.new(code_length) {COLORS.sample}
+  end
 end
